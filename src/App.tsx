d83c71ee55d35f5cb1d1e398c8c7533d01269f48
Phillip-Kalmanson <Phillip.Igor.Kalmanson@gmail.com>
2025-04-15 12:48:01 -0400

    customized

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { buttonVariants } from "@/components/ui/button";
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
-      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <main className="relative flex flex-col items-center justify-center min-h-screen space-y-20 overflow-hidden bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
+      {/* Background glow */}
+      <div className="absolute top-[-10rem] left-1/2 -translate-x-1/2 h-[30rem] w-[60rem] rounded-full bg-purple-700 opacity-20 blur-3xl pointer-events-none" />
+
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-center z-10">
         Vite, React, Shadcn-ui minimal starter
       </h1>
+      
       <a
-        href="https://github.com/moinulmoin/vite-react-tailwind-starter"
+        href="https://github.com/Phillip-Kalmanson/vite-react-tailwind-boilerplate"
         target="_blank"
         rel="noreferrer"
         className={buttonVariants()}
